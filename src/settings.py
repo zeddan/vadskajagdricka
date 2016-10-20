e8e1d5fd6652e9323d3ef53d436aaf30f499a115
Kalk <simon.d.mansson@gmail.com>
2016-10-20 13:05:36 +0200

    updated settings file

--- src/settings.py
+++ src/settings.py
@@ -1,2 +1,6 @@
+import json
 DEBUG = True
-SECRET_KEY = 'temporary_secret_key'  # make sure to change this
+SYSTEMET_KEY = "YOUR MASHAPE KEY HERE"
+VISION_KEY = "YOUR GOOGLE VISION API KEY HERE"
+EMOTION_MATRIX = open('src/json/emotionmatrix.json').read()
+CATEGORIES = open('src/json/categories.json').read()
