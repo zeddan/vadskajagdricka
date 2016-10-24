74ab442c3b02bf19845db434c1e17efaaf24c98c
Kalk <simon.d.mansson@gmail.com>
2016-10-24 15:40:59 +0200

    added categories

--- src/systembolaget.py
+++ src/systembolaget.py
@@ -3,13 +3,14 @@ import json
 import requests
 from numpy import interp
 from src import app
+categories = json.loads(app.config.get("CATEGORIES"))
 url = "https://karlroos-systemet.p.mashape.com/product"
-# config fil git ignore
 key = app.config.get('SYSTEMET_KEY')
 headers = {"X-Mashape-Key": key, "Accept": "application/json"}
 
 
-def get_beverage(imagedata, category):
+def get_beverage(imagedata):
+    category = categories[5]
     payload = map_values(category,
                          imagedata['emotionScore'],
                          imagedata['brightness'],
@@ -18,25 +19,20 @@ def get_beverage(imagedata, category):
     return res
 
 
+
 def map_values(category, emotion_score, brightness, ecological):
     new_dict = {}
     new_dict['tag'] = category['tag']
-    '''new_dict['alcohol_from'] = interp(brightness, [1, 100],
+    new_dict['alcohol_from'] = interp(brightness, [1, 100],
                                       [category['alcohol_from'],
                                       category['alcohol_to']])
-    new_dict['alcohol_to'] = category['alcohol_to']'''
-    '''new_dict['apk_from'] = category['apk_from']
-    new_dict['apk_to'] = category['apk_to']'''
+    new_dict['alcohol_to'] = category['alcohol_to']
     new_dict['price_from'] = interp(emotion_score, [1, 100],
                                     [category['price_from'],
                                      category['price_to']])
     new_dict['price_to'] = category['price_to']
-    # new_dict['ecological'] = ecological
+    new_dict['ecological'] = ecological
     new_dict['order'] = 'ASC'
     new_dict['order_by'] = 'price'
     new_dict["limit"] = 1
-    print()
-    print("NEW DICT")
-    print(new_dict)
-    print()
     return new_dict
