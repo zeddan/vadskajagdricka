922774b16f9092d82d6132da3aa4e04f43b0600b
Kalk <simon.d.mansson@gmail.com>
2016-10-24 15:42:41 +0200

    redone filter method and added check for face in analyse method

--- src/vision.py
+++ src/vision.py
@@ -7,6 +7,7 @@ url = 'https://vision.googleapis.com/v1/images:annotate?fields=responses&key='
 key = app.config.get('VISION_KEY')
 emotion_matrix = json.loads(app.config.get('EMOTION_MATRIX'))
 
+
 # takes a base64encode string represting an image
 # returns filterd response from google vision API
 def analyse(byte_string_image):
@@ -19,7 +20,7 @@ def analyse(byte_string_image):
                "features": [
                 {
                  "type": "FACE_DETECTION",
-                 "maxResults": 3
+                 "maxResults": 1
                 },
                 {
                  "type": "LABEL_DETECTION",
@@ -39,45 +40,40 @@ def analyse(byte_string_image):
               }
              ]
             }
-    response = requests.post(url+key, json=imageRequest)
-    result = filter(response.json())
-    return json.dumps(result)
+    response = requests.post(url+key, json=imageRequest).json()
+    if 'faceAnnotations' not in response['responses'][0]:
+            return {}, 422
+    else:
+        result = filter(response)
+        return json.dumps(result), 200
 
 
 # takes a response as input parameter returns new dictionary
 def filter(response):
     new_dict = {}
     res = response['responses'][0]
-    if 'faceAnnotations' not in res:
-        new_dict['message'] = """We couldn't find your face but thanks
-                               for a picture of the lovley""" + res['labelAnnotations'][0]['description']
-        return new_dict
-    else:
-        emotions = {}
-        persons = []
-        colors = []
-        faces = res['faceAnnotations']
-        for face in faces:
-            emotions['joy'] = face['joyLikelihood']
-            emotions['surprise'] = face['surpriseLikelihood']
-            emotions['headwear'] = face['headwearLikelihood']
-            emotions['sorrow'] = face['sorrowLikelihood']
-            emotions['anger'] = face['angerLikelihood']
-            emotions['emotionScore'] = calculate_score(emotions, face['detectionConfidence'])
-            persons.append(emotions)
-        new_dict['persons'] = persons
+    emotions = {}
+    colors = []
+    labels = []
+    face = res['faceAnnotations'][0]
+    emotions['joy'] = face['joyLikelihood']
+    emotions['surprise'] = face['surpriseLikelihood']
+    emotions['headwear'] = face['headwearLikelihood']
+    emotions['sorrow'] = face['sorrowLikelihood']
+    emotions['anger'] = face['angerLikelihood']
+    new_dict['emotionScore'] = calculate_score(emotions, face['detectionConfidence'])
 
-        for color in res['imagePropertiesAnnotation']['dominantColors']['colors']:
-            colors.append(color['color'])
+    for color in res['imagePropertiesAnnotation']['dominantColors']['colors']:
+        colors.append(color['color'])
         new_dict['brightness'] = calculate_brightness(colors)
-        
-        message = res['labelAnnotations'][0]['description']
-        new_dict['message'] = message
-        if res['safeSearchAnnotation']['adult'] == "VERY_UNLIKELY":
-            new_dict['ecological'] = "false"
-        else:
-            new_dict['ecological'] = "true"
-        return new_dict
+
+    labels.append(res['labelAnnotations'][0]['description'])
+    new_dict['labels'] = labels
+    if res['safeSearchAnnotation']['adult'] == "VERY_UNLIKELY":
+        new_dict['ecological'] = "false"
+    else:
+        new_dict['ecological'] = "true"
+    return new_dict
 
 
 # takes emotions and confidence as input and returns a score between 1 and 100
