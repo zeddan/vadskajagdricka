668bcb004907e9ea4e4037e136f0ce9d2d58eecb
zeddan <robin.saaf@gmail.com>
2016-10-25 10:58:35 +0200

    Upload settings.py again

---
+++ src/settings.py
@@ -0,0 +1,6 @@
+DEBUG = True
+SYSTEMET_KEY = ""
+VISION_KEY = ""
+EMOTION_MATRIX = open('src/json/emotionmatrix.json').read()
+CATEGORIES = open('src/json/categories.json').read()
+CATEGORIES_TIMES = open('src/json/categories_times.json').read()
