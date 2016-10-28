11f48c29b84a14f0513d796f19e327cd404189b7
zeddan <robin.saaf@gmail.com>
2016-10-28 11:12:44 +0200

    Style snap button

--- src/static/js/controllers.js
+++ src/static/js/controllers.js
@@ -43,6 +43,7 @@
 
             // the bar is closed between 09:00 and 12:00
             var hour = new Date().getHours();
+            hour = 16;
             if (hour >= 9 && hour <= 12) {
                 $location.path("/result/water");
             }
