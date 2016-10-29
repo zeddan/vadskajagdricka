b54e59665e5e95d90b523c1f1be2bc370147583a
zeddan <robin.saaf@gmail.com>
2016-10-29 16:50:17 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,5 @@
-DEBUG = True
-SYSTEMET_KEY = ""
-VISION_KEY = ""
+SYSTEMET_KEY = "add your mashape key here"
+VISION_KEY = "add your google cloud key here"
 EMOTION_MATRIX = open('src/json/emotionmatrix.json').read()
 CATEGORIES = open('src/json/categories.json').read()
 CATEGORIES_TIMES = open('src/json/categories_times.json').read()
